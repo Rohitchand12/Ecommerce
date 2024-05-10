1daf1f611d08c7190b7ce270f2abe221dedbabb5
RohitChand12 <rohitchand490@gmail.com>
2024-05-10 22:37:33 +0530

    refactor - mounted category router middleware for category route

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const authRouter = require('./routes/authRoutes')
 const cartRouter = require('./routes/cartRoutes')
 const productRouter = require('./routes/productRoutes')
 const orderRouter = require('./routes/orderRoutes')
+const categoryRouter = require('./routes/categoryRoutes')
 const app = express();
 
 app.use(express.json());
@@ -14,5 +15,6 @@ app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/products',productRouter);
 app.use('/api/v1/cart',cartRouter);
 app.use('/api/v1/orders',orderRouter);
+app.use('/api/v1/category',categoryRouter);
 
 module.exports = app;
