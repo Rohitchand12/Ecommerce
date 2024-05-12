0529731edd2c719188336011f95cd67cacc3a12c
RohitChand12 <rohitchand490@gmail.com>
2024-05-12 13:09:02 +0530

    refactor - added global error handling middleware and reviews route

--- server/app.js
+++ server/app.js
@@ -1,20 +1,32 @@
 const express = require("express");
-const authRouter = require('./routes/authRoutes')
-const cartRouter = require('./routes/cartRoutes')
-const productRouter = require('./routes/productRoutes')
-const orderRouter = require('./routes/orderRoutes')
-const categoryRouter = require('./routes/categoryRoutes')
+const authRouter = require("./routes/authRoutes");
+const cartRouter = require("./routes/cartRoutes");
+const productRouter = require("./routes/productRoutes");
+const orderRouter = require("./routes/orderRoutes");
+const reviewRouter = require("./routes/reviewRoute");
+const globalErrorHandler = require('./middlewares/errorMiddleware/globalErrorHandler');
+const AppError = require("./utils/appError");
 const app = express();
 
 app.use(express.json());
 
-//Mounting routes 
-console.log(process.env.NODE_ENV);
- 
-app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/products',productRouter);
-app.use('/api/v1/cart',cartRouter);
-app.use('/api/v1/orders',orderRouter);
-app.use('/api/v1/category',categoryRouter);
+//Mounting routes middleware
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/reviews", reviewRouter);
+
+
+// handling random routes error
+
+app.all('*',(req,res,next)=>{
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+})
+
+//error handling middleware
+
+app.use(globalErrorHandler);
 
 module.exports = app;
