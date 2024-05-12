5b7bbd46aa47cc6577be8cb0e0d3711bd23bbbc6
RohitChand12 <rohitchand490@gmail.com>
2024-05-12 13:10:19 +0530

    refactor - added error handling in DB connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-const mongoose = require('mongoose');
+dotenv.config({ path: "./.env" });
+const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB = process.env.DB_URI.replace('<password>',process.env.DB_PASSWORD);
-mongoose.connect(DB,{dbName:'Ecommerce_db'}).then(()=>console.log("db connected successfully"))
-
+const DB = process.env.DB_URI.replace("<password>", process.env.DB_PASSWORD);
+mongoose
+  .connect(DB, { dbName: "Ecommerce_db" })
+  .then(() => console.log("db connected successfully"))
+  .catch((e) => console.log(e));
 
 const port = process.env.PORT;
 app.listen(port, () => {
