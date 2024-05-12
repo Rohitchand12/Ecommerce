428abab3bf131adac0979aa4d1b6a9303809ae76
RohitChand12 <rohitchand490@gmail.com>
2024-05-12 13:12:30 +0530

    refactor - product Schema refactored

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,37 +1,90 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const productSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:[true,'Product title is required'],
-        // validate: [validator.isAlphanumeric , 'Title should contains only alphabets and digits'],
-        trim:true
-    },
-    description:{
-        type:String,
-        required:[true,'Please add product description'],
-        trim:true
-    },
-    images:{
-        type:[String]
-    },
-    brand:{
-        type:String,
-        required:[true,'Brand is required'],
-        trim:true
-    },
-    category:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Category',
-        required:[true,'Product category is required'],
-        trim:true
-    },
-    color:{
-        type:String,
-        trim:true
-    }
+const mongoose = require("mongoose");
 
-},{timestamps:true});
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Product title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please add product description"],
+      trim: true,
+    },
+    highlights: [
+      {
+        type: String,
+        required: [true, "proceed to add a highlight"],
+      },
+    ],
+    specifications: [
+      {
+        title: {
+          type: String,
+          required: [true, "specification title is required"],
+        },
+        description: {
+          type: String,
+          required: [true, "specification description is required"],
+        },
+      },
+    ],
+    original_price: {
+      type: Number,
+      required: [true, "original Price is required"],
+    },
+    sale_price: {
+      type: Number,
+    },
+    coverImage: {
+      type: String,
+      required: [true, "cover image of product is required"],
+    },
+    images: [
+      {
+        url: {
+          type: String,
+          required: [true, "images url required"],
+        },
+      },
+    ],
+    brand: {
+      type: String,
+      required: [true, "Brand is required"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+    },
+    color: {
+      type: String,
+      trim: true,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    quantity: {
+      type: Number,
+      required: [true, "quantity is required"],
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
-const Product = mongoose.model('Product',productSchema);
+//virtual populate
+
+productSchema.virtual("reviews",{
+    ref:'Review',
+    foreignField:'product',
+    localField:'_id'
+});
+
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
