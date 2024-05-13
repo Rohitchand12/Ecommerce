19fd0f95f047535ec03388a87d205407524be81c
RohitChand12 <rohitchand490@gmail.com>
2024-05-13 07:13:01 +0530

    refactor - handled unhandled rejections and exceptions on server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./.env" });
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+process.on('uncaughtException',(err)=>{
+  console.log(err.name , err.message);
+  console.log("UNCAUGHT EXCEPTION 💣 Shutting down ... ");
+  process.exit(1); 
+})
+
+
+dotenv.config({ path: "./.env" });
 const app = require("./app");
 
 const DB = process.env.DB_URI.replace("<password>", process.env.DB_PASSWORD);
 mongoose
   .connect(DB, { dbName: "Ecommerce_db" })
   .then(() => console.log("db connected successfully"))
-  .catch((e) => console.log(e));
+
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app running on port ${port}.`);
 });
+
+process.on('unhandledRejection',(err)=>{
+  console.log(err.name , err.message);
+  console.log("UNHANDLED ERROR 💣 Shutting down ... ");
+  server.close(()=>{
+    process.exit(1)
+  })
+})
