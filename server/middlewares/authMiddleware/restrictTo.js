f6111f28051d52e9bf8537c214cd1ac889233d95
RohitChand12 <rohitchand490@gmail.com>
2024-05-14 07:14:32 +0530

    feat - authorization to admin and users

---
+++ server/middlewares/authMiddleware/restrictTo.js
@@ -0,0 +1,18 @@
+const AppError = require("../../utils/appError");
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // this is a closure ,so it will have access to roles
+    //roles = ['admin','user'] whatever is passed
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You donot have permission to perform  this action"),
+        403
+      ); //403 -> forbidden
+    }
+    next();
+  };
+};
+
+//  in above if statement , we write req.user.role bcoz we use restrictTo middleware after protect
+// and in protect ,at last wrote req.user = currentUser , hence user is available in req
