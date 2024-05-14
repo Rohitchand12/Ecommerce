ac327734425c59e5f50d9f84ddc505ab695f8900
RohitChand12 <rohitchand490@gmail.com>
2024-05-14 07:19:34 +0530

    feat - protected routes

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const productsController = require("../controllers/productsController");
 const productRouter = express.Router();
+const {protect} = require('../middlewares/authMiddleware/protect');
+const {restrictTo} = require('../middlewares/authMiddleware/restrictTo');
 
 //defining routes
 
 productRouter
   .route("/")
   .get(productsController.getAllProducts)
-  .post(productsController.postProduct);
+  .post(protect,restrictTo('admin'),productsController.postProduct);
   
 productRouter
   .route("/:id")
   .get(productsController.getProduct)
-  .patch(productsController.updateProduct)
-  .delete(productsController.deleteProduct);
+  .patch(protect,restrictTo('admin'),productsController.updateProduct)
+  .delete(protect,restrictTo('admin'),productsController.deleteProduct);
 
 module.exports = productRouter;
