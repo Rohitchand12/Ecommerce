6a3e6962ed2948e2d4285ecf4fae9e82a3bbf553
RohitChand12 <rohitchand490@gmail.com>
2024-05-17 15:20:04 +0530

    Add - security headers, rate limiter, data sanitization

--- server/app.js
+++ server/app.js
@@ -6,9 +6,41 @@ const orderRouter = require("./routes/orderRoutes");
 const reviewRouter = require("./routes/reviewRoute");
 const globalErrorHandler = require('./middlewares/errorMiddleware/globalErrorHandler');
 const AppError = require("./utils/appError");
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize')
+const hpp = require('hpp');
+
+//instantiating app
 const app = express();
 
-app.use(express.json());
+
+//GLOBAL MIDDLEWARES
+
+// app.use(express.json({limit:'10kb'})) can be used to limit body data
+app.use(express.json()); //parsing data from body
+app.use(helmet()) // security http headers
+
+
+const limiter = rateLimit({
+    max:100, //100 requests per 15 minutes
+    windowMs : 15*60*60*1000, //15 min window
+    message:'Too many requests , please try again later'
+})
+
+app.use('/api',limiter); // rate limiting
+
+//Data sanitization
+app.use(mongoSanitize()); //nosql injection
+
+app.use(hpp()) // handling parameter pollution
+//in order to allow duplicate parameter properties , whitelist the properties
+// app.use(hpp({
+//     whitelist:[
+//         duration , ...
+//     ]
+// }))
+
 
 //Mounting routes middleware
 
