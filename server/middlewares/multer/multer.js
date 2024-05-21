840a5f69cc38d2a240d7d6e8e4bbb4308217013f
RohitChand12 <rohitchand490@gmail.com>
2024-05-21 09:00:38 +0530

    Add -  multer for recieving files in request

---
+++ server/middlewares/multer/multer.js
@@ -0,0 +1,14 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'./public/uploads');
+    },
+    filename:function(req,file,cb){
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
+        cb(null,`${uniqueSuffix}-${file.originalname}`);
+    }
+})
+
+const upload = multer({storage});
+export default upload;
