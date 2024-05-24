3016f7522b31b1bd986a9b803ee6a3ce5c32055f
RohitChand12 <rohitchand490@gmail.com>
2024-05-24 07:27:37 +0530

    initial commit

---
+++ client/src/app/product/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Product
---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
+
+function Menu() {
+  return (
+    <div>
+        <RxHamburgerMenu/>
+    </div>
+  )
+}
+
+export default Menu
