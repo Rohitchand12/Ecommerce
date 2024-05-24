1c146d900952d74d3ee347da37d84871cbec23a8
RohitChand12 <rohitchand490@gmail.com>
2024-05-24 07:33:22 +0530

    Refactor - add file filters

--- server/middlewares/multer/multer.js
+++ server/middlewares/multer/multer.js
@@ -1,14 +1,22 @@
-import multer from 'multer';
+import multer from "multer";
+import AppError from "../../utils/appError.js";
 
 const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,'./public/uploads');
-    },
-    filename:function(req,file,cb){
-        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
-        cb(null,`${uniqueSuffix}-${file.originalname}`);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "./public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
+});
 
-const upload = multer({storage});
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("please upload only images."), false);
+  }
+};
+const upload = multer({ storage , fileFilter : multerFilter });
 export default upload;
