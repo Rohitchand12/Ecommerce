00e1dfc466b8522251b7224d9640101b6d528632
RohitChand12 <rohitchand490@gmail.com>
2024-06-27 07:54:44 +0530

    ADD - cart route , order route

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import { addToCart, getAllCarts, getCart } from "../controllers/cartControllers.js";
+import { protect } from "../middlewares/authMiddleware/protect.js";
 const cartRouter = Router();
+
+cartRouter.route('/').get(protect,getCart).post(protect,addToCart);
+
 export default cartRouter;
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,3 +1,7 @@
 import { Router } from "express";
+import {createOrder, getOrders} from "../controllers/ordersController.js"
+import { protect } from "../middlewares/authMiddleware/protect.js";
 const orderRouter = Router();
+
+orderRouter.route("/").get(getOrders).post(protect,createOrder)
 export default orderRouter;
