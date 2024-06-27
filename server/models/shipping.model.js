aa4c986f965958c8548209e31e7fe60bc238a5b3
RohitChand12 <rohitchand490@gmail.com>
2024-06-27 07:56:19 +0530

    ADD - shipping Schema

---
+++ server/models/shipping.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export const shippingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Shipping details must belong to a user"],
+    },
+    address: {
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      pinCode: { type: String, required: true },
+      country: { type: String, required: true },
+    }
+  },
+  { timestamps: true }
+);
+
+const Shipping = mongoose.model("Shipping", shippingSchema);
+export default Shipping;
