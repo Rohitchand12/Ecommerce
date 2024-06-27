bee0c394a326074695960ffb89e12dccde1b2d03
RohitChand12 <rohitchand490@gmail.com>
2024-06-27 07:55:56 +0530

    Refactor - auth route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout, forgotPassword, resetPassword } from '../controllers/authController.js';
+import { signup, login, logout, forgotPassword, resetPassword, getProfile } from '../controllers/authController.js';
 import upload from "../middlewares/multer/multer.js";
 import { protect } from "../middlewares/authMiddleware/protect.js";
 const authRouter = express.Router();
@@ -10,5 +10,5 @@ authRouter.route('/login').post(login);
 authRouter.route('/logout').post(protect,logout);
 authRouter.route('/forgotPassword').post(forgotPassword);
 authRouter.route('/resetPassword/:token').post(resetPassword);
-
+authRouter.route('/profile').get(protect,getProfile);
 export default authRouter;
