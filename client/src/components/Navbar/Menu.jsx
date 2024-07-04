3c900cf47d43ce6087d1b6968b4215dc7787c54f
RohitChand12 <rohitchand490@gmail.com>
2024-07-04 12:12:09 +0530

    Refactor - navlinks

--- client/src/components/Navbar/Menu.jsx
+++ client/src/components/Navbar/Menu.jsx
@@ -7,13 +7,13 @@ import { RxHamburgerMenu } from "react-icons/rx";
 function Menu() {
   const [menuOpen,setMenuOpen] = useState(false);
   return (
-    <div className='center '>
+    <div className='center'>
         <button onClick = {()=>setMenuOpen(prev => !prev)}><RxHamburgerMenu/></button>
         {
           menuOpen &&  (
-            <div className='absolute w-full h-[calc(100vh-80px)] top-20 left-0 z-10 flex-col text-xl gap-8 center bg-black bg-opacity-30 backdrop-blur-md'>
+            <div className='absolute w-full h-[calc(100vh-80px)] top-20 left-0 z-10 flex-col text-xl gap-8 center bg-black bg-opacity-30 backdrop-blur-md z-10'> 
               <Link href="/">Home</Link>
-              <Link href="/">Shop</Link>
+              <Link href="/products">Shop</Link>
               <Link href="/">About</Link>
               <Link href="/">Contact</Link>
             </div>
