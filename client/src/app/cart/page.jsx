e6e394950fba8db82f8b0f7c616c34f8c4873b68
RohitChand12 <rohitchand490@gmail.com>
2024-07-06 18:42:05 +0530

    Refactor - debug cart page

--- client/src/app/cart/page.jsx
+++ client/src/app/cart/page.jsx
@@ -1,10 +1,16 @@
 import fetchCart from "@/libs/fetchCart"
 async function Cart() {
   const cart = await fetchCart();
-  console.log(cart);
+ 
   return (
     <div>
-      Cart
+      {
+        cart.data.cart.items.map(item=>{
+          return (
+            <p key = {item.product._id}>{item.product.title}</p>
+          )
+        })
+      }
     </div>
   )
 }
