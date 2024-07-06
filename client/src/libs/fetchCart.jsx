09e62c4d57043aaaac87f8da5d7fa0adae56039d
RohitChand12 <rohitchand490@gmail.com>
2024-07-06 18:57:36 +0530

    Refactor - changed axios to fetch in fetchcart

--- client/src/libs/fetchCart.jsx
+++ client/src/libs/fetchCart.jsx
@@ -1,13 +1,20 @@
-import axios from "axios";
 
 async function fetchCart() {
   try {
-    const response = await axios.get("https://api.mystickart.online/api/v1/cart", {
-      withCredentials: true,
+    const response = await fetch("https://api.mystickart.online/api/v1/cart", {
+      credentials: "include",
     });
     // const response = await axios.get("https://mystickart.onrender.com/api/v1/cart",{withCredentials:true});
-    console.log("response from fetch -> ",response);
-    return response.data;
+    if (!response.ok) {
+      const err = await response.json();
+      throw new Error(err);
+    }
+
+    // console.log("response from axios -> ",response.data);
+    // return response.data;
+    const data = await response.json();
+    console.log("response from fetch -> ",data);
+    
   } catch (error) {
     console.log("error is ---->",error);
   }
