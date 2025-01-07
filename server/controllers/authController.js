e562b0e305efebeed1856acfb4b46f6b2685a0fb
RohitChand12 <rohitchand490@gmail.com>
2025-01-07 11:56:34 +0530

    testing 2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,13 +28,12 @@ const sendJWTResponse = (user, statusCode, res) => {
     ),
     httpOnly: true,
     path: "/",
-    sameSite:"None"
   };
-  // if (process.env.NODE_ENV === "production") {
-  //   cookieOptions.domain = ".mystickart.online";
-  //   cookieOptions.secure = true;
-  //   cookieOptions.sameSite = "None";
-  // }
+  if (process.env.NODE_ENV === "production") {
+    // cookieOptions.domain = ".mystickart.online";
+    cookieOptions.secure = true;
+    cookieOptions.sameSite = "None";
+  }
   const cookieOptionsIsAuth = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
